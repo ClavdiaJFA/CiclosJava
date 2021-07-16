2bacc476b16dc69e9fd079829d5b3607cd8ee898
ClavdiaJFA <cjfa08102013@gmail.com>
2021-07-16 10:51:19 -0500

    Ciclos Java

---
+++ CiclosJava/src/CiclosJava.java
@@ -0,0 +1,26 @@
+
+public class CiclosJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		
+		/*
+		 * Ciclo For
+		 * for( variable de inicializacion; variable inicializacion <10; variable ++
+		 * 
+		 */
+		
+		for(int i=0; i < 5; i++) {
+			System.out.println(i + 1);
+		}
+
+		
+	}
+
+}
